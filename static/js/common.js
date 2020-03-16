8eb863c641f415d0fd0aaff96ce762197364f914
Kevin Toh <kevintoh0305@gmail.com>
2020-03-16 19:04:34 +0800

    Starting Login Screen

--- static/js/common.js
+++ static/js/common.js
@@ -2,28 +2,25 @@ $(document).ready(function () {
     $(".carousel").carousel({
         interval: 7500
     });
-    $(".owl-carousel").owlCarousel(
-        {
-            loop: true,
-            margin: 10,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false,
-                    loop: false,
-                },
-                768: {
-                    items: 2,
-                    nav: false,
-                    loop: false,
-                },
-                1000: {
-                    items: 4,
-                    nav: false,
-                    loop: false,
-                },
-            }
-        }
-    );
+    $(".owl-carousel").owlCarousel({
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                loop: false,
+            },
+            768: {
+                items: 2,
+                nav: false,
+                loop: false,
+            },
+            1000: {
+                items: 4,
+                nav: false,
+                loop: false,
+            },
+        },
+    });
 });
