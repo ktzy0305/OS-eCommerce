ed67cc171d724ed2635a84f7e8449134d59694b3
Kevin Toh <kevintoh0305@gmail.com>
2021-10-15 17:01:54 +0800

    Revamp checkout

--- static/js/shoppingcart.js
+++ static/js/shoppingcart.js
@@ -3,21 +3,37 @@ function decrementValue(index){
     let value_input = document.getElementsByName("product_quantity")[index-1];
     var value = parseInt(value_input.value, 10);
     value = isNaN(value) ? 0 : value;
-    if(value > 0){
+    if(value > 1){
         value--;
+        value_input.value = value;
+        form.submit();
     }
-    value_input.value = value;
-    form.submit();
 }
 
 function incrementValue(index){
     let form = document.getElementsByTagName("form")[index];
-    let value_input = document.getElementsByName("product_quantity")[index-1] 
+    let value_input = document.getElementsByName("product_quantity")[index-1];
     var value = parseInt(value_input.value, 10);
     value = isNaN(value) ? 0 : value;
     if(value < 10){
         value++;
+        value_input.value = value;
+        form.submit();
     }
-    value_input.value = value;
-    form.submit();
+}
+
+function validateQuantity(index){
+    let form = document.getElementsByTagName("form")[index];
+    let value_input = document.getElementsByName("product_quantity")[index-1];
+    var value = parseInt(value_input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value > 0 && value <= 10){
+        console.log("Valid");
+        value_input.classList.remove("is-invalid");
+        value_input.value = value;
+        form.submit();
+    }
+    else{
+        value_input.classList.add("is-invalid");
+    }
 }
